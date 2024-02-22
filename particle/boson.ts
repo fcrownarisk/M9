53b9252be095f1a5680fdc238bdeedf339ca799a
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-22 14:55:58 +0800

    Update boson.ts

--- particle/boson.ts
+++ particle/boson.ts
@@ -1,23 +1,23 @@
 class Particle1 {
     name: string
     quantumNumber: number
-    constructor(name: string, quantumNumber: number) {
+    constructor(name: string, quantity: number) {
         this.name = name
-        this.quantumNumber = quantumNumber
+        this.quantity = quantity
     }
     sameStateParticles: Particle1[]
     transitionState(newState: number): void {
         console.log(`${this.name} transitioned to state${newState}`);
-        this.quantumNumber = newState
+        this.quantity = newState
     }
 }
 class Boson extends Particle1 {
     sameStateParticles: Particle1[] = []
-constructor(name: string, quantumNumber: number) {
+constructor(name: string, quantity: number) {
         super(name, quantumNumber)
     }
     occupySameState(anotherBoson: Boson): void {
-        this.transitionState(anotherBoson.quantumNumber); // Use the method
+        this.transitionState(anotherBoson.quantity); // Use the method
         this.sameStateParticles.push(anotherBoson);
         console.log(`${this.name} and ${anotherBoson.name} are now in the same state.`);
     }
