c8b7f37bbafb9fd1481519464d6c3d7129f265cf
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-22 14:58:10 +0800

    Update fermion.ts

--- particle/fermion.ts
+++ particle/fermion.ts
@@ -2,13 +2,13 @@
 class Particle2 {
     name: string
     quantumNumber: number
-    constructor(name: string, quantityr: number) {
+    constructor(name: string, quantity: number) {
         this.name = name
         this.quantity = quantity
     }
     transitionState(newState: number): void {
         console.log(`${this.name} transitioned to state${newState}`)
-        this.quantityr = newState
+        this.quantity = newState
     }
 }
 class Fermion extends Particle2 {
